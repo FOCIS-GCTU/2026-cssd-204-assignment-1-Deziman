a1449beeedcf4f9916d9a5e40a1a8e3e75cef4e8
Deziman <2425403696@live.gctu.edu.gh>
2026-05-30 23:10:07 +0000

    DAD.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
 
+# File: DAD.py
+# Description: This is a python code to print out my initials with print functions and strings
+# Assignment Number: 2
+#
+# Name: DENNIS ADU DENKYIRA
+# STUDENT ID: 2425403696
+# Email: 2425403696@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Dennis Adu Denkyira , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 def main():
-  pass #code goes here
-
-
+#this is the main function
+	print("")
+	print("...DAD")
+	print("")
+	print("...DDDDDDDDD..............AAAA...........DDDDDDDDD.......")
+	print("...DD.......DD...........AA..AA..........DD.......DD.....")
+	print("...DD........DD.........AA....AA.........DD........DD....")
+	print("...DD........DD........AA......AA........DD........DD....")
+	print("...DD........DD.......AA........AA.......DD........DD....")
+	print("...DD........DD.......AAAAAAAAAAAA.......DD........DD....")
+	print("...DD........DD.......AA........AA.......DD........DD....")
+	print("...DD........DD.......AA........AA.......DD........DD....")
+	print("...DD.......DD...**...AA........AA..**...DD.......DD...**")
+	print("...DDDDDDDDD.....**...AA........AA..**...DDDDDDDDD.....**")
+	print("")
 
 main()
